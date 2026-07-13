a56e51d2cd134c9a16ce9e1fc2e0efb74cdbded7
kit-trowbridge <kittrow.dev@gmail.com>
2026-07-13 11:06:46 +0100

    initial project setup

---
+++ app/src/main/java/com/example/filmquote/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.filmquote
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.filmquote.ui.theme.FilmQuoteTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FilmQuoteTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(), // is this necessary? without it, text not vertically aligned
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    FilmCard(
+                        quote = "\"Yeah, well, you know, that's just, like, your opinion, man.\"",
+                        title = "The Big Lebowski"
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun FilmCard(quote: String, title: String, modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        modifier = modifier
+            .padding(all = 30.dp)
+    ) {
+        Text(
+            text = quote,
+            fontSize = 50.sp,
+            lineHeight = 50.sp
+        )
+        Spacer(
+            Modifier.height(25.dp)
+        )
+        Text(
+            text = title,
+            fontSize = 30.sp,
+            modifier = Modifier
+                .align(alignment = Alignment.End)
+        )
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    FilmCard(
+        quote = "\"Yeah, well, you know, that's just, like, your opinion, man.\"",
+        title = "The Big Lebowski"
+    )
+}
